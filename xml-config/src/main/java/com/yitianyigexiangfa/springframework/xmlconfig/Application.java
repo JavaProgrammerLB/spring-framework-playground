ea0245504dde324dff196d29dfeb7a329298b4a7
刘备 <admin@yitianyigexiangfa.com>
2019-07-28 09:44:10 +0800

    去除所有的xml配置，完全使用注解配置

--- xml-config/src/main/java/com/yitianyigexiangfa/springframework/xmlconfig/Application.java
+++ xml-config/src/main/java/com/yitianyigexiangfa/springframework/xmlconfig/Application.java
@@ -1,17 +1,18 @@
 package com.yitianyigexiangfa.springframework.xmlconfig;
 
 import com.yitianyigexiangfa.springframework.xmlconfig.entity.Boss;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Bill Lau
  * @date 2019-07-28
  */
+@ComponentScan(value = "com.yitianyigexiangfa.springframework.xmlconfig")
 public class Application {
 
     public static void main(String[] args) {
-       String[] locations = new String[]{"spring.xml"};
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(locations);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         Boss boss = (Boss)context.getBean("boss");
         boss.selfIntroduce();
         context.close();
